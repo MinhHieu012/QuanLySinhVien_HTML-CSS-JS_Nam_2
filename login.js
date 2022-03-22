a35a7556d8205b54e017cca79571c80780bb266d
MinhHieu012 <0967710509hieu@gmail.com>
2022-03-22 14:39:17 +0700

    update

---
+++ login.js
@@ -0,0 +1,12 @@
+// function validate() {
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+//     if (username == 'admin@gmail.com' && password == '0967710509') {
+//         elert("Đăng nhập thành công");
+//         // window.open("google.com");
+//         return false;
+//     }
+//     else {
+//         alert("Email hoặc Mật khẩu không đúng ");
+//     }
+// }
